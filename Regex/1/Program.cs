174a526c23c241d4a517b5b49015a00e134d9221
KainaTPetrova <k_alp@abv.bg>
2020-12-06 23:48:45 +0200

    add files

---
+++ Regex/1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @">>(\w+)<<(\d+\.?\d*)!(\d+)";
+
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+
+            Console.WriteLine($"Bought furniture:");
+            double sum = 0;
+            while (input != "Purchase")
+            {
+                Match match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    string name = match.Groups[1].Value;
+                    double price = double.Parse(match.Groups[2].Value);
+                    int q = int.Parse(match.Groups[3].Value);
+                     sum += price * q;
+                    Console.WriteLine(name);
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total money spend: {sum:f2}");
+        }
+    }
+}
---
+++ Regex/2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            Dictionary<string, int> racers = new Dictionary<string, int>();
+            string namePattern = @"[\W\d]";
+            string numPattern = @"[\WA-z]";
+            string input = Console.ReadLine();
+            foreach (var name in names)
+            {
+                racers.Add(name, 0);
+            }
+            while (input!="end of race")
+            {
+
+                string name = Regex.Replace(input, namePattern, "");
+                string dist = Regex.Replace(input, numPattern, "");
+                int sum = 0;
+                foreach (var item in dist)
+                {
+                    sum += int.Parse(item.ToString());
+                }
+                if (racers.ContainsKey(name))
+                {
+                racers[name] += sum;
+
+                }
+                
+
+                input = Console.ReadLine();
+            }
+            int count = 1;
+            foreach (var item in racers.OrderByDescending(x=>x.Value))
+            {
+                string text = count == 1 ? "st" : count == 2 ? "nd" : "rd";
+
+                Console.WriteLine($"{count++}{text} place: {item.Key}");
+                if (count == 4)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
