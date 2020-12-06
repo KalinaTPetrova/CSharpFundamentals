b57c9181733120fe5ec1c9f426d1633207d689bb
KainaTPetrova <k_alp@abv.bg>
2020-12-06 23:33:59 +0200

    add files

---
+++ CsharpFundamentalsExamPrep/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+namespace CsharpFundamentalsExamPrep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            string input = Console.ReadLine();
+
+            while (input != "Reveal")
+            {
+                string[] cmds = input.Split(":|:");
+                string command = cmds[0];
+
+                switch (command)
+                {
+                    case "InsertSpace":
+                        int index = int.Parse(cmds[1]);
+                        message = message.Insert(index, " ");
+                        Console.WriteLine(message);
+                        break;
+                    case "Reverse":
+                        string substring = cmds[1];
+                        if (!message.Contains(substring))
+                        {
+                            Console.WriteLine("error");
+                        }
+                        else
+                        {
+                            int ind = message.IndexOf(substring);
+
+                            string reversed = Reverse(substring);
+
+                            message = message.Remove(ind, substring.Length);
+                            message = message.Insert(message.Length, reversed);
+                            Console.WriteLine(message);
+                        }
+                        break;
+                    case "ChangeAll":
+                        string sub = cmds[1];
+                        string repl = cmds[2];
+                        message = message.Replace(sub, repl);
+                        Console.WriteLine(message);
+                        break;
+                    default:
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"You have a new text message: {message}");
+        }
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ ExamPrep_MirrorWords/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ExamPrep_MirrorWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(@|#)([a-zA-Z]{3,})\1\1([a-zA-Z]{3,})\1";
+            
+            string input = Console.ReadLine();
+
+            var validInput = Regex.Matches(input, pattern);
+
+            var mirror = new List<string>();
+           
+            if (validInput.Count == 0)
+            {
+                Console.WriteLine("No word pairs found!");
+            }
+            else
+            {
+                Console.WriteLine($"{validInput.Count} word pairs found!");
+
+                foreach (Match current in validInput)
+                {
+                    string one = current.Groups[2].Value;
+                    string two = current.Groups[3].Value;
+
+                    string toCompare = Reverse(two);
+
+                    if (one == toCompare)
+                    {
+                        mirror.Add($"{one} <=> {two}");
+                    }
+                   
+                }                          
+                                
+            }
+            if (mirror.Count == 0)
+            {
+                Console.WriteLine("No mirror words!");
+            }
+            else
+            {
+                Console.WriteLine("The mirror words are:");
+                Console.WriteLine(string.Join(", ", mirror));
+            }
+        }
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
