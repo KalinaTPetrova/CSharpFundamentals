fb7db2e99f9bc269b23733c7765bb9a9c9324034
KainaTPetrova <k_alp@abv.bg>
2020-12-10 02:02:06 +0200

    add files

---
+++ ExamPrep_PlantDiscovery/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExamPrep_PlantDiscovery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary <string, Plant> plants = new Dictionary<string, Plant>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split("<->", StringSplitOptions.RemoveEmptyEntries);
+
+                Plant plant = new Plant(input[0], int.Parse(input[1]), new List<double> ());
+                plants[input[0]]=plant;
+                if (plants.ContainsKey(input[0]))
+                {
+                    plants[input[0]].Update(int.Parse(input[1]));
+                }
+            }
+
+            string command = Console.ReadLine();
+            while (command != "Exhibition")
+            {
+
+                string[] splitters = new string[] { ": ", " - " };
+                string[] cmdArgs = command.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+                string cmd = cmdArgs[0];
+                string plnt = cmdArgs[1];
+
+                if (!plants.ContainsKey(plnt))
+                {
+                    Console.WriteLine("error");
+                    command = Console.ReadLine();
+
+                    continue;
+                }
+
+                if (cmd == "Rate")
+                {                    
+                    plants[plnt].Rate(double.Parse(cmdArgs[2]));                 
+                    
+                }
+                else if (cmd == "Update")
+                {
+                    plants[plnt].Update(int.Parse(cmdArgs[2]));
+                }
+                else if (cmd == "Reset")
+                {
+                    plants[plnt].Reset();
+                }
+               
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("Plants for the exhibition:");
+            foreach (var item in plants.OrderByDescending(x => x.Value.Rarity).ThenByDescending(x => x.Value.Rating.Sum() / x.Value.Rating.Count))
+            {
+                Console.WriteLine(item.Value);
+            }            
+
+        }
+        public class Plant
+        {
+            public Plant(string name, int rarity, List<double> rating)
+            {
+                Name = name;
+                Rarity = rarity;
+                Rating = rating;
+
+            }
+            public string Name { get; set; }
+            public int Rarity { get; set; }
+            public List<double> Rating { get; set; }
+
+
+            public override string ToString()
+            {
+                double average = Rating.Count > 0 ? (Rating.Sum() / Rating.Count) : 0;
+                return $"- {Name}; Rarity: {Rarity}; Rating: {average:f2}";
+            }
+
+            public void Rate (double rating)
+            {
+                Rating.Add(rating);
+                
+            }
+
+            public void Update(int rarity)
+            {
+                Rarity = rarity;
+            }
+
+            public void Reset()
+            {
+                Rating.Clear();
+            }
+
+        }
+    }
+}
+    
+
---
+++ ExamPrep_PlantDiscovery_Alternative/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExamPrep_PlantDiscovery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var plants = new Dictionary<string, int>();
+            var ratings = new Dictionary<string, List<double>>();
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split("<->", StringSplitOptions.RemoveEmptyEntries);
+                string plant = input[0];
+                string rarity = input[1];
+
+                plants[plant] = int.Parse(rarity);
+                ratings[plant] = new List<double>();
+
+            }
+
+            string command = Console.ReadLine();
+            while (command != "Exhibition")
+            {
+
+                string[] splitters = new string[] { ": ", " - " };
+                string[] cmdArgs = command.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+                string cmd = cmdArgs[0];
+                string plnt = cmdArgs[1];
+
+                if(!plants.ContainsKey(plnt)&&!ratings.ContainsKey(plnt))
+                {
+                    Console.WriteLine("error");
+                    command = Console.ReadLine();
+                    continue;
+                }
+                if (cmd == "Rate")
+                {
+                    ratings[plnt].Add(double.Parse(cmdArgs[2]));
+                }
+                else if (cmd == "Update")
+                {
+                    plants[plnt] = int.Parse(cmdArgs[2]);
+                }
+                else if (cmd == "Reset")
+                {
+                    ratings[plnt].Clear();
+                }
+                
+
+                command = Console.ReadLine();
+            }
+
+            Dictionary<string, double> average = new Dictionary<string, double>();
+            foreach (var item in ratings)
+            {
+                double av = 0;
+                if (item.Value.Count > 0)
+                {
+                    av = item.Value.Sum() / item.Value.Count;
+                }
+                average.Add(item.Key, av);
+            }
+
+            Console.WriteLine("Plants for the exhibition:");
+            foreach (var rarity in plants.OrderByDescending(x => x.Value).ThenByDescending(x => average[x.Key]))
+            {
+                Console.WriteLine($"- {rarity.Key}; Rarity: {rarity.Value}; Rating: {average[rarity.Key]:f2}");
+
+            }
+
+        }
+    }
+}
