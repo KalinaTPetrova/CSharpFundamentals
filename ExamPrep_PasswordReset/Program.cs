9590aa926cb11c676b3f3b8cd1c52e13c3f4dcc4
KainaTPetrova <k_alp@abv.bg>
2020-12-11 14:58:47 +0200

    add file

---
+++ ExamPrep_PasswordReset/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExamPrep_PasswordReset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string password = "";
+            string commands = Console.ReadLine();
+            while (commands != "Done")
+            {
+                string[] cmdArgs = commands.Split();
+                string command = cmdArgs[0];
+
+                switch (command)
+                {
+                    case "TakeOdd":
+                        for (int i = 0; i < input.Length; i++)
+                        {
+                            if (i % 2 == 1)
+                            {
+                                password += input[i];
+                                
+                            }
+                        }
+                                Console.WriteLine(password);
+                        break;
+                    case "Cut":
+                        break;
+                    case "Substitute":
+                        break;
+                    default:
+                        break;
+                }
+                commands = Console.ReadLine();
+            }
+        }
+    }
+}
